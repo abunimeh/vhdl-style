4da68d6b17b0b20580d40ed8799e39b5b97c564d
Tristan Gingold <tristan.gingold@cern.ch>
2018-03-20 17:14:06 +0100

    Improve check_name_decl rule: can now give a dedicated error message.

--- vhdllint/syntaxrules/check_name_decl.py
+++ vhdllint/syntaxrules/check_name_decl.py
@@ -19,17 +19,37 @@ class CheckNameDecl(SyntaxNodeRule):
         self.kind = kind
         self.predicate = predicate
 
+    def check_predicate(self, p, node, s):
+        """Test predicate :param p: on string :param s:.  The predicate shall
+         return an error message in case of error or None if the name is
+         correct."""
+        res = p(s)
+        if isinstance(res, str):
+            self.error(Location.from_node(node), res)
+            return True
+        elif res is None or res:
+            return False
+        else:
+            self.error(Location.from_node(node),
+                       "incorrect name '{}'".format(s))
+            return True
+
     def check(self, input, node):
         if iirs.Get_Kind(node) == self.kind:
             s = nodeutils.get_identifier_str(node)
-            if not self.predicate(s):
-                self.error(Location.from_node(node),
-                           "incorrect name '{}'".format(s))
+            if isinstance(self.predicate, list):
+                for p in self.predicate:
+                    if self.check_predicate(p, node, s):
+                        break
+            else:
+                self.check_predicate(self.predicate, node, s)
 
     @staticmethod
     def test(runner):
-        rule = CheckNameDecl(kind=iirs.Iir_Kind.Constant_Declaration,
-                             predicate=(lambda s: s != 'Reserved'))
+        rule = CheckNameDecl(
+            kind=iirs.Iir_Kind.Constant_Declaration,
+            predicate=(lambda s: ("name {} must not be 'Reserved'".format(s)
+                                  if s == 'Reserved' else None)))
         TestRunOK(runner, "File without attributes",
                   rule, "hello.vhdl")
         TestRunFail(runner, "Incorrect name",
